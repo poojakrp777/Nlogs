59bdf810c16af144729d2d607ae6a8bc998654ba
poojakrp777 <poojakrp777@gmail.com>
2022-10-10 16:27:17 +0530

    Add Nlog project

---
+++ Addition.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlog_Demo
+{
+    internal class Addition
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void Sum(int first, int second)
+        {
+            if (first == 0 || second == 0)
+            {
+                logger.Warn("please provide +ve Value");
+                logger.Error("Number should not be below zero");
+                logger.Debug("Debug failed");
+            }
+
+            int sum = first + second;
+            logger.Debug("Debugging with sum operation successfull");
+            logger.Info("sum:" + sum);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlog_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter first number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Plese enter second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Addition.Sum(first, second);
+        }
+    }
+}
