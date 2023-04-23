a0326eaf6fd571bc984310c7a9c0f997242d0ce1
poojakrp777 <poojakrp777@gmail.com>
2023-04-23 12:11:50 +0530

    N-Log Problems

--- Addition.cs
+++ Addition.cs
@@ -25,5 +25,25 @@ namespace Nlog_Demo
             logger.Info("sum:" + sum);
 
         }
+        public static void pattern()
+        {
+            int length = 7, width = 7;
+            for (int i = 1; i <= width; i++)
+            {
+                for(int j=1; j <= length;j++)
+                {
+                    if (i == 1 || i == width || j == 1 || j == length)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    
+                }
+                Console.Write("\n");
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,12 +10,14 @@ namespace Nlog_Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Please enter first number");
-            int first = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Plese enter second number");
-            int second = Convert.ToInt32(Console.ReadLine());
-            Addition.Sum(first, second);
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Please enter first number");
+            //int first = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Plese enter second number");
+            //int second = Convert.ToInt32(Console.ReadLine());
+            //Addition.Sum(first, second);
+            Addition.pattern();
+            Console.ReadLine();
         }
     }
 }
